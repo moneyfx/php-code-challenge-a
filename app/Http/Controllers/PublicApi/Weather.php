b630b5892199f879cedb3548c09fd985d9aa8e15
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-02-04 00:07:43 +0000

    show temperature by ip

--- app/Http/Controllers/PublicApi/Weather.php
+++ app/Http/Controllers/PublicApi/Weather.php
@@ -3,13 +3,31 @@
 namespace App\Http\Controllers\PublicApi;
 
 use App\Http\Controllers\Controller;
+use Gmopx\LaravelOWM\LaravelOWM;
 
 class Weather extends Controller
 {
-    public function index()
+    public function index($ip = '')
     {
+    	$geoip = geoip($ip);
+    	$lat = $geoip->getAttribute('lat');
+    	$lon = $geoip->getAttribute('lon');
+
+    	$lowm = new LaravelOWM();
+    	$currentWeather = $lowm->getCurrentWeather([
+    		'lat' => $lat,
+            'lon' => $lon,
+            'metric'
+        ]);
+
         return response()->json([
-            'ip' => '8.8.8.8',
+            'ip' => $geoip->getAttribute('ip'),
+            'city' => $currentWeather->city->name,
+            'temperature' => [
+                'current' => $currentWeather->temperature->now->getValue(),
+                'low' => $currentWeather->temperature->min->getValue(),
+                'high' => $currentWeather->temperature->max->getValue(),
+            ]
         ]);
     }
 }
