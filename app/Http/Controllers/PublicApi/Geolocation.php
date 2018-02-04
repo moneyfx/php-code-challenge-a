4d268b60c87784c741970b7e2c8b3ece1e2ef065
Mani Ahmadi Arsham <moneyfx@gmail.com>
2018-02-04 03:50:40 +0000

    refactor - move ipApi call from controller to service class

--- app/Http/Controllers/PublicApi/Geolocation.php
+++ app/Http/Controllers/PublicApi/Geolocation.php
@@ -3,25 +3,32 @@
 namespace App\Http\Controllers\PublicApi;
 
 use App\Http\Controllers\Controller;
+use App\Service\IpApi as IpApiService;
 
 class Geolocation extends Controller
 {
+    private $ipApiService;
+
+    public function __construct(IpApiService $ipApiService)
+    {
+        $this->ipApiService = $ipApiService;
+    }
+
     public function index($ip = '')
     {
     	if (empty($ip)) {
             $ip = \Request::ip();
     	}
         
-    	//**@var Torann\GeoIP\Location
-    	$geoip = geoip($ip);
+        $location = $this->ipApiService->getLocation($ip);
 
         return response()->json([
-            'ip' => $geoip->getAttribute('ip'),
+            'ip' => $location->getAttribute('ip'),
             'geo' => [
             	'service' => 'ip-api',
-            	'city' => $geoip->getAttribute('city'),
-            	'region' => $geoip->getAttribute('state_name'),
-            	'country' => $geoip->getAttribute('country'),
+            	'city' => $location->getAttribute('city'),
+            	'region' => $location->getAttribute('state_name'),
+            	'country' => $location->getAttribute('country'),
             ]
         ]);
     }
